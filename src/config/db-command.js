c58d82db1cdbcd27bfd48e8d9e7efa1845e6cd77
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-05 14:59:03 +0900

    #3 채팅 로그 조회 기능 구현

---
+++ src/config/db-command.js
@@ -0,0 +1,26 @@
+// MongoDB Command
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/chatting";
+
+exports.insertChattingLog = (nickname, message) => {
+  MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("chatting");
+
+    const chattingLog = {
+      nickname: nickname,
+      message: message,
+      time: new Date().toLocaleString().toString(),
+    };
+
+    dbo
+      .collection("logs")
+      .find({})
+      .sort({ time: 1 })
+      .toArray((err, res) => {
+        if (err) throw err;
+        console.log(res);
+        db.close();
+      });
+  });
+};
